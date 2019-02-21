06519515ca2fd1d48fac3b6589ad535d3bfb6f1d
user <user@MK08>
2019-02-21 14:52:49 +0800

    second

--- src/d0588678.java
+++ src/d0588678.java
@@ -3,7 +3,7 @@ public class d0588678 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO FCU");
+		System.out.println("HELLO world");
 
 	}
 
